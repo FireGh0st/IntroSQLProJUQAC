a77bd2559bee1792d938cbcbc1255f4d34af816f
Lilian HATTON <lilian.hatton@epita.fr>
2024-04-19 21:33:27 -0400

    univ push

---
+++ univ.sql
@@ -0,0 +1,7 @@
+NouvelleAireStationnement('Université du Québec à Montréal', 'UQAM','405' ,'Rue Sainte-Catherine Est', 'Montréal', 'Québec', 'H2L2C4');
+NouvelleAireStationnement('Université du Québec en Outaouais', 'UQO','283' ,'Boul. Alexandre-Taché', 'Gatineau', 'Québec', 'J8X3X7');
+NouvelleAireStationnement('Université du Québec à Rimouski', 'UQAR','300' ,'All. des Ursulines', 'Rimouski', 'Québec', 'G5L3A1');
+NouvelleAireStationnement('Université du Québec en Abitibi-Témiscamingue', 'UQAT','445' ,'Bd de l\'Université', 'Rouyn-Noranda', 'Québec', 'J9X5E4');
+NouvelleAireStationnement('Université du Québec à Trois-Rivières', 'UQTR','3351' ,'Bd des Forges', 'Trois-Rivières', 'Québec', 'G8Z4M3');
+NouvelleAireStationnement('Université du Québec à Chicoutimi', 'UQAC','555' ,'Bd de l\'Université', 'Chicoutimi', 'Québec', 'G7H2B1');
+NouvelleAireStationnement('Université Laval', 'ULaval','2325' ,'Rue de l\'Université', 'Québec', 'Québec', 'G1V0A6');
